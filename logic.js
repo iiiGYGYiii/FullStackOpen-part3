bc0016de129f3125f11506555fd8cede7cc68507
iiiGYGYiii <realGYGY@gmail.com>
2021-04-28 00:51:57 -0500

    part3 a)NodejS... exercises 3.1-3.8

---
+++ logic.js
@@ -0,0 +1,33 @@
+const elementExists = (array, field, element) =>{
+    return array.filter(el=>el[field]==element[field])[0];
+};
+const invalidInput = ( input, size=2 ) =>{
+    return Object.keys(input).length!==size;
+}
+const validateRequest = ({
+    array,
+    field,
+    request,
+    size=2
+}) => {
+    return elementExists(array, field, request) ?
+    { error: 'field must be unique'}:
+    invalidInput(request, size) ?
+    { error: 'fields are required (name, number)'}:
+    undefined;
+}
+
+const updateArrayByField = (array, value, field) => array.map(el => el[field]===value[field] ? value : el);
+
+const deleteElementByField = (array, value, field) => array.filter(el=> el[field]!=value[field]);
+
+const getNextId = (array) => Math.max(...array.map(el=>el.id))+1;
+
+module.exports = {
+    validateRequest,
+    getNextId,
+    elementExists,
+    invalidInput,
+    updateArrayByField,
+    deleteElementByField
+}
--- server.js
+++ server.js
@@ -1,14 +1,98 @@
+// MODULES
+let {persons} = require('./data.js');
+const { updateArrayByField,
+    validateRequest,
+    getNextId,
+    elementExists,
+    invalidInput,
+    deleteElementByField } = require('./logic');
 const express = require('express');
-const morgan = require('morgan');
-
 const app = express();
-app.use(express.json());
+const morgan = require('morgan');
+morgan.token('body', (req, res) => JSON.stringify(req.body));
 
+//MIDDLEWARES
+app.use(express.json());
+app.use(morgan( ':method :url :status :res[content-length] - :response-time ms :body' ));
+
+// ROUTING
 app.route("/")
 .get((req,res)=>{
     res.send("<h1> Hello World!</h1>");
 });
 
-const PORT = process.env.PORT ? process.env.PORT : 3001;
+app.route("/api/persons")
+.get((req,res)=>{
+    res.json(persons);
+})
+.post((req,res)=>{
+    const isInvalidRequest = validateRequest({
+        array: persons,
+        field: 'name',
+        request: req.body,
+        size: 2
+    });
+    if (isInvalidRequest){
+        res.status(400).json(isInvalidRequest).end();
+        return;
+    }
+    const newPerson = {
+        ...req.body,
+        id: getNextId(persons)
+    };
+    persons = [...persons, newPerson];
+    res.json(newPerson);
+});
 
+app.get("/info", (req,res)=>{
+    const date = new Date().toISOString();
+    res.send(`<p>Phonebook has info for ${persons.length} people </p><p>${date}</p>`);
+});
+
+app.route("/api/persons/:id")
+.get((req,res)=>{
+    const person = elementExists(persons, 'id', req.params);
+    person ? res.json(person) : res.status(404).end();
+})
+.put((req,res)=>{
+    const person = elementExists(persons, 'id', req.params);
+    if(!person){
+        res.status(404).end();
+        return;
+    }
+    if(invalidInput(req.body)){
+        res.status(400).end();
+        return;
+    }
+    const putUser = {
+        ...req.body,
+        id: Number(req.params.id)
+    }
+    persons = updateArrayByField(persons, putUser, 'id');
+    res.json(putUser);
+})
+.patch((req,res)=>{
+    let person = elementExists(persons, 'id', req.params);
+    if (!person){
+        res.status(404).end();
+        return;
+    }
+    if (invalidInput(req.body, 1)){
+        res.status(400).end();
+        return;
+    }
+    person = {
+        ...person,
+        ...req.body
+    };
+    persons = updateArrayByField(persons, person, 'id');
+    res.json(person);
+
+})
+.delete((req,res)=>{
+    persons = deleteElementByField(persons, req.params, 'id');
+    res.status(204).end();
+});
+
+const PORT = process.env.PORT ? process.env.PORT : 3001;
 app.listen(PORT, () =>{console.log(`Running server on ${PORT}`);});
